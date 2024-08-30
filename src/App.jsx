d0154421d40f21140070fac4a1fa4e5543178604
Oswaldo Palacios Perez <yaco2002@live.com.mx>
2024-08-30 09:21:50 -0600

    add marquee

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/autoplay";
-import { Autoplay } from "swiper/modules";
 import Navbar from "./Components/Navbar";
-import avatar from './assets/avatar.png';
-import js from './assets/js.svg';
-import ts from './assets/ts.svg';
-import pg from './assets/pg.svg';
-import redux from './assets/redux.svg';
-import react from './assets/react.svg';
-import tailwind from './assets/tailwind.svg';
-import next from './assets/next.svg';
-import sq from './assets/sq.png';
-import my from './assets/my.svg';
-import css from './assets/css.svg';
-import node from './assets/node.svg';
+import avatar from "./assets/avatar.png";
+import js from "./assets/js.svg";
+import ts from "./assets/ts.svg";
+import pg from "./assets/pg.svg";
+import redux from "./assets/redux.svg";
+import react from "./assets/react.svg";
+import tailwind from "./assets/tailwind.svg";
+import next from "./assets/next.svg";
+import sq from "./assets/sq.png";
+import my from "./assets/my.svg";
+import css from "./assets/css.svg";
+import node from "./assets/node.svg";
+import Marquee from "react-marquee-slider";
+
 
 function App() {
   return (
@@ -27,7 +26,9 @@ function App() {
         </p>
         <h1
           className="text-[#490581] sm:text-6xl text-2xl sm:text-left text-center font-orbitron font-bold mt-10"
-          style={{ textShadow: "0 0 8px #b517ff, 0 0 10px #b517ff, 0 0 15px #b517ff" }}
+          style={{
+            textShadow: "0 0 8px #b517ff, 0 0 10px #b517ff, 0 0 15px #b517ff",
+          }}
         >
           FRONT END DEVELOPER
         </h1>
@@ -39,41 +40,38 @@ function App() {
       </div>
 
       <div className="technologies absolute sm:left-28 left-4 sm:mr-0 sm:top-[350px] top-[705px]  w-[360px] sm:w-[600px]">
-        <p className="bg-[#243dafa9] text-blue-400 rounded-lg font-audiowide text-4xl text-center p-1 w-[290px] m-auto" style={{ textShadow: "0 0 1px #54c1f8, 0 0 4px #54c1f8, 0 0 1px #54c1f8" }}>
+        <p
+          className="bg-[#243dafa9] text-blue-400 rounded-lg font-audiowide text-4xl text-center p-1 w-[290px] m-auto"
+          style={{
+            textShadow: "0 0 1px #54c1f8, 0 0 4px #54c1f8, 0 0 1px #54c1f8",
+          }}
+        >
           Technologies
         </p>
-        <p className="text-white font-audiowide text-center mt-4 text-2xl">Frontend</p>
-        <Swiper
-          modules={[Autoplay]}
-          spaceBetween={0}
-          slidesPerView={5}
-          loop={true}
-          autoplay={{ delay: 500, disableOnInteraction: false }}
-          className="mt-4 sm:ml-24"
-        >
-          <SwiperSlide><img className="w-[50px] m-0 " src={js} alt="JavaScript" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={react} alt="React" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={ts} alt="TypeScript" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={redux} alt="Redux" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={css} alt="CSS" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={next} alt="Next.js" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={tailwind} alt="Tailwind CSS" /></SwiperSlide>
-        </Swiper>
-        
-        <p className="text-white font-audiowide text-center mt-5 text-2xl">Backend</p>
-        <Swiper
-          modules={[Autoplay]}
-          spaceBetween={0}
-          slidesPerView={3}
-          loop={true}
-          autoplay={{ delay: 500, disableOnInteraction: false }}
-          className="mt-4 sm:ml-24"
-        >
-          <SwiperSlide><img className="w-[50px]" src={node} alt="Node.js" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={pg} alt="PostgreSQL" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={sq} alt="Sequelize" /></SwiperSlide>
-          <SwiperSlide><img className="w-[50px]" src={my} alt="MySQL" /></SwiperSlide>
-        </Swiper>
+        <p className="text-white font-audiowide text-center mt-4 text-2xl">
+          Frontend
+        </p>
+        <div className="flex justify-center  w-[450px] m-auto ">
+          <Marquee velocity={50} minScale={0.5} resetAfterTries={200} >
+          <img className="w-[50px] sm:mr-3 " src={js} alt="JavaScript" />
+          <img className="w-[50px] sm:mr-3" src={react} alt="React" />
+          <img className="w-[50px] sm:mr-3" src={ts} alt="TypeScript" />
+          <img className="w-[50px] sm:mr-3" src={redux} alt="Redux" />
+          <img className="w-[50px] sm:mr-3" src={css} alt="CSS" />
+          <img className="w-[50px] sm:mr-3" src={next} alt="Next.js" />
+          <img className="w-[50px]" src={tailwind} alt="Tailwind CSS" />
+          </Marquee>
+        </div>
+
+        <p className="text-white font-audiowide text-center mt-5 text-2xl">
+          Backend
+        </p>
+        <div className="flex justify-center">
+          <img className="w-[50px]" src={node} alt="Node.js" />
+          <img className="w-[50px]" src={pg} alt="PostgreSQL" />
+          <img className="w-[50px]" src={sq} alt="Sequelize" />
+          <img className="w-[50px]" src={my} alt="MySQL" />
+        </div>
       </div>
     </div>
   );
